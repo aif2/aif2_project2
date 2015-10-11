b8b0b7a3fc9012da450c62d7a8dc7b1b2451ad9a
AFelimban <aif2@zips.uakron.edu>
2015-10-11 01:27:42 -0400

    Project2 version1

---
+++ aif2_project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aif2_project2
+//
+//  Created by Anhar Felimban on 10/8/15.
+//  Copyright © 2015 Anhar Felimban. All rights reserved.
+//  aif2@zips.uakron.edu
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

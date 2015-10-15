9a56ff55da5bda95bb8662e051b0d0c7614a8328
AFelimban <aif2@zips.uakron.edu>
2015-10-14 23:47:33 -0400

    Add reference outlets and retrieve data from plist

--- aif2_project2/ViewController.swift
+++ aif2_project2/ViewController.swift
@@ -10,20 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var setTitle: UITextField!
+    @IBOutlet weak var setAuther: UITextField!
+    @IBOutlet weak var setISBN: UITextField!
+    @IBOutlet weak var setPuplisher: UITextField!
+    @IBOutlet weak var setPupDate: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
+        let path = NSBundle.mainBundle().pathForResource("Info", ofType: "plist")
+        let dictionary = NSDictionary(contentsOfFile: path!)
         
+        setTitle.text = dictionary!.valueForKey("title") as? String
+        setAuther.text = dictionary!.valueForKey("auther") as? String
+        setISBN.text = dictionary!.valueForKey("ISBN") as? String
+        setPuplisher.text = dictionary!.valueForKey("puplisher") as? String
+        setPupDate.text = dictionary!.valueForKey("puplicationdate ") as? String
         
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBOutlet weak var setTitle: UILabel!
-    
+ 
     
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
